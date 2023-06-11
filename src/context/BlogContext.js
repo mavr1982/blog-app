27bdd5b09428688eaf65d3b25cdc26797818d598
Miguel Valdepenas VFC <miguel_valdepenas@vfc.com>
2023-06-11 17:30:52 +0200

    lesson 141

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import createDataContext from "./createDataContext";
 
-const BlogContext = React.createContext();
-
-export const BlogProvider = ({ children }) => {
-    const [blogPosts, setBlogPosts] = useState([])
-
-    const addBlogPost = () => {
-        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}]);
-    }
-
-    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
-        {children}
-    </BlogContext.Provider>
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [
+                ...state,
+                { 
+                    id: Math.floor(Math.random() * 99999),
+                    title: `Blog post #${state.length + 1}`
+                }
+            ];
+        case 'delete_blogpost':
+            return state.filter((blogpost) => blogpost.id !== action.payload)
+        default:
+            return state;
+    };
 };
 
-export default BlogContext;
+const addBlogPost = (dispatch) => {
+    return () => {
+        dispatch({ type: 'add_blogpost' });
+    };
+};
+
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id });
+    };
+};
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, []);
